4cfdeaa051e9d3e81b433e50f56d3fb2ad03d010
SePhiRoTh-exe <roby-9991@hotmail.it>
2021-12-27 12:59:24 +0100

    aggiunta servlet logout

---
+++ src/main/java/control/LogoutServlet.java
@@ -0,0 +1,30 @@
+package control;
+
+import model.UserBean;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("./Home.jsp");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}

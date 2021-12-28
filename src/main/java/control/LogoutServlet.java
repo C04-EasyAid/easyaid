dbee9bad41386a14cc53603ff415208926151b44
SePhiRoTh-exe <roby-9991@hotmail.it>
2021-12-28 15:33:06 +0100

    aggiunti commenti alle classi bean, alle servlet ed aggiunte le annotazioni web servlet

--- src/main/java/control/LogoutServlet.java
+++ src/main/java/control/LogoutServlet.java
@@ -3,12 +3,18 @@ package control;
 import model.bean.UserBean;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * @author Roberto Tartaglia
+ * Servlet che permette di effettuare il logout
+ */
+@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 

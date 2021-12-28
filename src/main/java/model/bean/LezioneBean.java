8ad45d3ec063e4059bf60e3fb2dfd210f57492dd
serenaliguori <serenaliguori28@gmail.com>
2021-12-28 16:56:45 +0100

    Modifica LezioneDAO e LezioneBean per l'aggiunta dell'id

--- src/main/java/model/bean/LezioneBean.java
+++ src/main/java/model/bean/LezioneBean.java
@@ -7,11 +7,21 @@ Classe LezioneBean
  */
 public class LezioneBean {
   // Variabili d'istanza
-  private int tutorato;
+  private int id,tutorato;
   private String oraInizio, oraFine, tutor;
   private Date data;
   private boolean status;
-
+ //Costruttore con id
+  public LezioneBean(
+          int id,int tutorato, String oraInizio, String oraFine, Date data, String tutor, boolean status) {
+    this.id = id;
+    this.tutorato = tutorato;
+    this.oraInizio = oraInizio;
+    this.oraFine = oraFine;
+    this.tutor = tutor;
+    this.data = data;
+    this.status = status;
+  }
   // Costruttore
   public LezioneBean(
       int tutorato, String oraInizio, String oraFine, Date data, String tutor, boolean status) {
@@ -29,6 +39,15 @@ public class LezioneBean {
   }
 
   // Metodi
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
   public int getTutorato() {
     return tutorato;
   }
--- src/main/java/model/dao/LezioneDAO.java
+++ src/main/java/model/dao/LezioneDAO.java
@@ -30,9 +30,9 @@ public class LezioneDAO {
             stmt = conn.prepareStatement(query);
             stmt.setString(1,Integer.toString(id));
             ResultSet rs = stmt.executeQuery();
-            LezioneBean bean = new LezioneBean();
+            LezioneBean bean = null;
             while (rs.next()) {
-                bean=new LezioneBean(rs.getInt("tutorato"),rs.getString("ora_inizio"), rs.getString("ora_fine"), rs.getDate("data"), rs.getString("tutor"), rs.getBoolean("status"));
+                bean=new LezioneBean(rs.getInt("id"),rs.getInt("tutorato"),rs.getString("ora_inizio"), rs.getString("ora_fine"), rs.getDate("data"), rs.getString("tutor"), rs.getBoolean("status"));
                 lezioni.add(bean);
             }
         } catch (SQLException e) {

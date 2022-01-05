907d5af3112c8125439d2ae606b60654208df154
giovanni-toriello <g.toriello@studenti.unisa.it>
2022-01-05 14:25:29 +0100

    Aggiunto metodo LezioneDAO per verificare che la richiesta sia completata

--- src/main/java/model/dao/LezioneDAO.java
+++ src/main/java/model/dao/LezioneDAO.java
@@ -1,14 +1,17 @@
 package model.dao;
 
 import model.bean.LezioneBean;
-import model.bean.UserBean;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import static model.dao.ConnectionPool.conn;
 
@@ -24,7 +27,8 @@ public class LezioneDAO {
     LezioneBean bean = null;
     PreparedStatement stmt = null;
     String query =
-        "SELECT * from easyaid.lezione L inner join tutorato_didattico T where L.tutorato = T.idtutorato_didattico AND L.tutorato =" + id;
+        "SELECT * from easyaid.lezione L inner join tutorato_didattico T where L.tutorato = T.idtutorato_didattico AND L.tutorato ="
+            + id;
     try {
       conn = conn();
       stmt = conn.prepareStatement(query);
@@ -53,7 +57,45 @@ public class LezioneDAO {
     return bean;
   }
 
-  //Metodo che restituisce tutte le lezioni di un determinato studente il quale è stato passato come parametro
+  public synchronized Collection<LezioneBean> doRetrieveLezioniById(int id)
+      throws ClassNotFoundException, SQLException {
+    Connection conn = null;
+    LezioneBean bean = null;
+    PreparedStatement stmt = null;
+    Collection<LezioneBean> lezioni = new ArrayList<>();
+    String query =
+        "SELECT * from easyaid.lezione L inner join tutorato_didattico T where L.tutorato = T.idtutorato_didattico AND L.tutorato ="
+            + id;
+    try {
+      conn = conn();
+      stmt = conn.prepareStatement(query);
+      ResultSet rs = stmt.executeQuery();
+      while (rs.next()) {
+        bean =
+            new LezioneBean(
+                rs.getInt("id"),
+                rs.getInt("tutorato"),
+                rs.getString("ora_inizio"),
+                rs.getString("ora_fine"),
+                rs.getDate("data"),
+                rs.getString("tutor"),
+                rs.getBoolean("status"));
+        bean.setInsegnamento(rs.getString("insegnamento"));
+        bean.setEmailStudente(rs.getString("studente_email"));
+        lezioni.add(bean);
+      }
+    } catch (SQLException e) {
+
+    } finally {
+      if (stmt != null) stmt.close();
+      if (conn != null) {
+        conn.close();
+      }
+    }
+    return lezioni;
+  }
+  // Metodo che restituisce tutte le lezioni di un determinato studente il quale è stato passato
+  // come parametro
   public synchronized Collection<LezioneBean> doRetrieveLezioneByStudente(String studente)
       throws ClassNotFoundException, SQLException {
     Connection conn = null;
@@ -70,13 +112,13 @@ public class LezioneDAO {
       while (rs.next()) {
         lezione =
             new LezioneBean(
-                    rs.getInt("id"),
-                    rs.getInt("tutorato"),
-                    rs.getString("ora_inizio").substring(0,5),
-                    rs.getString("ora_fine").substring(0,5),
-                    rs.getDate("data"),
-                    rs.getString("tutor"),
-                    rs.getBoolean("status"));
+                rs.getInt("id"),
+                rs.getInt("tutorato"),
+                rs.getString("ora_inizio").substring(0, 5),
+                rs.getString("ora_fine").substring(0, 5),
+                rs.getDate("data"),
+                rs.getString("tutor"),
+                rs.getBoolean("status"));
         lezione.setInsegnamento(rs.getString("insegnamento"));
         lezione.setEmailStudente(rs.getString("studente_email"));
         lezioni.add(lezione);
@@ -92,7 +134,8 @@ public class LezioneDAO {
     return lezioni;
   }
 
-  //Metodo che restituisce tutte le lezioni di un determinato tutor il quale è stato passato come parametro
+  // Metodo che restituisce tutte le lezioni di un determinato tutor il quale è stato passato come
+  // parametro
   public synchronized Collection<LezioneBean> doRetrieveLezioneByTutor(String tutor)
       throws ClassNotFoundException, SQLException {
     Connection conn = null;
@@ -111,8 +154,8 @@ public class LezioneDAO {
             new LezioneBean(
                 rs.getInt("id"),
                 rs.getInt("tutorato"),
-                rs.getString("ora_inizio").substring(0,5),
-                rs.getString("ora_fine").substring(0,5),
+                rs.getString("ora_inizio").substring(0, 5),
+                rs.getString("ora_fine").substring(0, 5),
                 rs.getDate("data"),
                 rs.getString("tutor"),
                 rs.getBoolean("status"));
@@ -130,6 +173,7 @@ public class LezioneDAO {
     }
     return lezioni;
   }
+
   public synchronized int doRetrieveTutoratoByLezione(int lezione) throws SQLException {
     int idTutorato = 0;
     Connection conn = null;
@@ -153,4 +197,40 @@ public class LezioneDAO {
     }
     return idTutorato;
   }
+//Metodo che restituisce un booleano
+// true = Se la somma delle ore delle lezioni sono uguali alle ore richieste
+// false = Se la somma delle ore delle lezioni non sono uguali alle ore richieste
+  private synchronized boolean countOre(Collection<LezioneBean> lezioni) throws SQLException, ClassNotFoundException {
+    boolean uguale = false;
+    int count = 0;
+    List<LezioneBean> lezioniS = (List<LezioneBean>) lezioni;
+    TutoratoDidatticoDAO dao = new TutoratoDidatticoDAO();
+    int idTutorato = doRetrieveTutoratoByLezione(lezioniS.get(0).getId());
+    int oreRichieste = dao.doRetriveById(idTutorato).getOreRichieste();
+    for (LezioneBean x : lezioni) {
+      String oraInizio = x.getOraInizio();
+      String oraFine = x.getOraFine();
+      int g = 0;
+      long t = 0;
+      SimpleDateFormat dateParser = new SimpleDateFormat("HH:mm");
+      {
+        try {
+          Date inizio = dateParser.parse(oraInizio);
+          Date fine = dateParser.parse(oraFine);
+          t = fine.getTime() - inizio.getTime();
+          int minutes = (int) ((t / 1000) / 60);
+          g = minutes / 60;
+
+        } catch (ParseException e) {
+          e.printStackTrace();
+        }
+      }
+      count+= g;
+    }
+    if(count==oreRichieste){
+      uguale=true;
+    }
+    return uguale;
+  }
+
 }

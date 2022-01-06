139685ac21cc3f55625f45279003965c1626b5ef
SePhiRoTh-exe <roby-9991@hotmail.it>
2022-01-06 23:06:29 +0100

    Aggiunta JSP profilo personale

--- src/main/java/control/LoginServlet.java
+++ src/main/java/control/LoginServlet.java
@@ -7,29 +7,30 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.Serial;
+
 import model.bean.UserBean;
 import model.dao.UserDAO;
 import other.MyLogger;
 
-/**
- * @author Roberto Tartaglia
- * Servlet che permette di effettuare il login
- */
+/** @author Roberto Tartaglia Servlet che permette di effettuare il login */
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+  @Serial
   private static final long serialVersionUID = 1L;
-private static MyLogger log = MyLogger.getInstance();
-private static String myClass = "LoginServlet";
+  private static final MyLogger log = MyLogger.getInstance();
+
   public LoginServlet() {
     super();
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
-    log.info(myClass,"Collegamento alla Servlet...");
+    String myClass = "LoginServlet";
+    log.info(myClass, "Collegamento alla Servlet...");
     HttpSession session = request.getSession();
     UserBean user = new UserBean();
-    UserDAO dao=new UserDAO();
+    UserDAO dao = new UserDAO();
     user.setEmail(request.getParameter("Email"));
     user.setPassword(request.getParameter("Password"));
     try {
@@ -38,14 +39,13 @@ private static String myClass = "LoginServlet";
         // Nessun utente nel sistema
         response.sendRedirect("view/LoginPage.jsp");
         session.setAttribute("alertMsg", "L'operazione non e' andata a buon fine");
-      }
-      else
-      {
+      } else {
         session.setAttribute("utente", user);
         response.sendRedirect("view/HomePage.jsp");
       }
     } catch (Throwable e) {
-      log.error(myClass,"Catturata eccezione nella Servlet", (Exception) e);
+      assert e instanceof Exception;
+      log.error(myClass, "Catturata eccezione nella Servlet", (Exception) e);
       session.setAttribute("alertMsg", "L'operazione non e' andata a buon fine");
       response.sendRedirect("view/LoginPage.jsp");
     }

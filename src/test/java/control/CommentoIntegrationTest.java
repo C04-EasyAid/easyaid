f0ffed7b5b93e0d31204594202c8bda0934024b3
Marti <m.giugliano41@studenti.unisa.it>
2022-01-19 22:32:30 +0100

    Realizzazione Test di Integrazione per CommentoIntegrationTest.java e InserimentoNuovaLezioneIntegrationTest.java

--- src/test/java/control/CommentoIntegrationTest.java
+++ src/test/java/control/CommentoIntegrationTest.java
@@ -1,28 +1,22 @@
 package control;
 
-import model.bean.LezioneBean;
-import model.bean.StudenteBean;
-import model.bean.TutorBean;
-import model.bean.UserBean;
+import model.bean.*;
 import model.dao.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Objects;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 
 /*
 @author Martina Giugliano
  */
+
 class CommentoIntegrationTest {
   private CommentoServlet servlet;
   private MockHttpServletRequest request;
@@ -37,16 +31,14 @@ class CommentoIntegrationTest {
 
   @Test
   void testInserimentoCommentoTutor3() throws ServletException, IOException, SQLException {
-    IUserDAO userDao = new UserDAO();
     UserBean userBean = new UserBean();
-    TutorBean tutorBean = new TutorBean();
 
     userBean.setEmail("lorenzorossi1@studenti.unisa.it");
     userBean.setPassword("Lorenzo#rossi1");
     userBean.setRuolo("T");
 
     LezioneBean lezione = new LezioneBean();
-    lezione.setId(4);
+    lezione.setId(16);
     request.getSession().setAttribute("utente", userBean);
     request.getSession().setAttribute("lezione", lezione);
     String msg = "Ok,Ci sarò!";
@@ -59,16 +51,16 @@ class CommentoIntegrationTest {
     commentoDao.insertCommentoTutor(lezione.getId(), msg, userBean.getEmail());
     servlet.doGet(request, response);
 
+    CommentoBean commento = new CommentoBean();
     assertEquals(
         "Commento inserito con successo",
         Objects.requireNonNull(request.getSession()).getAttribute("alertMsg"));
+        commentoDao.deleteCommento(new CommentoBean());
   }
 
   @Test
   void testInserimentoCommentoStudente3() throws ServletException, IOException, SQLException {
-    IUserDAO userDao = new UserDAO();
     UserBean userBean = new UserBean();
-    StudenteBean studenteBean = new StudenteBean();
 
     userBean.setEmail("abaglio9@studenti.unisa.it");
     userBean.setPassword("Aldo#Baglio45");
---
+++ src/test/java/control/InserimentoNuovaLezioneIntegrationTest.java
@@ -0,0 +1,82 @@
+package control;
+
+import model.bean.LezioneBean;
+import model.bean.TutorBean;
+import model.bean.UserBean;
+import model.dao.ILezioneDAO;
+import model.dao.IUserDAO;
+import model.dao.LezioneDAO;
+import model.dao.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+@author Martina Giugliano
+ */
+    class InserimentoNuovaLezioneIntegrationTest {
+        private InserimentoNuovaLezioneServlet servlet;
+        private MockHttpServletRequest request;
+        private MockHttpServletResponse response;
+
+        @BeforeEach
+        void setUp() {
+            servlet = new InserimentoNuovaLezioneServlet();
+            request = new MockHttpServletRequest();
+            response = new MockHttpServletResponse();
+        }
+
+        @Test
+        void testInserimentoNuovaLezione1() throws ParseException, ServletException, IOException {
+
+            UserBean userBean = new UserBean();
+            ILezioneDAO lezioneDAO = new LezioneDAO();
+            userBean.setNome("Lorenzo");
+            userBean.setCognome("Rossi");
+            userBean.setEmail("lorenzorossi1@studenti.unisa.it");
+            userBean.setPassword("Lorenzo#rossi1");
+            userBean.setRuolo("T");
+            request.getSession().setAttribute("utente", userBean);
+            int idTutorato = 18;
+            String oraInizio = "14:00";
+            String oraFine = "16:00";
+            String data = "2022-01-25";
+            String luogo = "Edificio F3 aula 404";
+            SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd");
+            request.setParameter("oraInizio", oraInizio);
+            request.setParameter("oraFine", oraFine);
+            request.setParameter("idTutorato", String.valueOf(idTutorato));
+            request.setParameter("data", data);
+            request.setParameter("luogo", luogo);
+            try {
+                Date date = dateParser.parse(data);
+                LezioneBean lezioneBean = new LezioneBean();
+                lezioneBean.setTutorato(idTutorato);
+                lezioneBean.setTutor(userBean.getEmail());
+                lezioneBean.setOraInizio(oraInizio);
+                lezioneBean.setOraFine(oraFine);
+                lezioneBean.setData(date);
+
+                servlet.setLezioneDAO(lezioneDAO);
+                servlet.doGet(request, response);
+                assertEquals(
+                        "Lezione Inserita",
+                        Objects.requireNonNull(request.getSession()).getAttribute("alertMsg"));
+                lezioneDAO.deleteLezione(lezioneBean);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+

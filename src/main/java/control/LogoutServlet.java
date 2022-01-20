b5162aca4b4248a9adf8e158c1e0d27afe530a68
giovanni-toriello <g.toriello@studenti.unisa.it>
2022-01-20 22:12:50 +0100

    Modifica javascript per form richiesta

--- src/main/java/control/LogoutServlet.java
+++ src/main/java/control/LogoutServlet.java
@@ -1,21 +1,20 @@
 package control;
 
-import java.io.IOException;
+import other.MyLogger;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import other.MyLogger;
+import java.io.IOException;
 
 /**
  * Servlet che permette di effettuare il logout.
  *
  * @author Roberto Tartaglia
- *
  */
-
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;

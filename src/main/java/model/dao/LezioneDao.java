7d9a9792d61bd8079c889c9d6fc698b4837d372f
giovanni-toriello <g.toriello@studenti.unisa.it>
2022-01-22 15:30:05 +0100

    Checkstyle Fix

--- src/main/java/model/dao/LezioneDao.java
+++ src/main/java/model/dao/LezioneDao.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import static model.dao.ConnectionPool.conn;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,7 +14,6 @@ import java.util.Date;
 import java.util.List;
 import model.bean.LezioneBean;
 
-import static model.dao.ConnectionPool.conn;
 
 /**
  * Classe ILezioneDAO.
@@ -20,7 +21,7 @@ import static model.dao.ConnectionPool.conn;
  * @author Serena Liguori
  */
 
-public class LezioneDAO implements ILezioneDAO {
+public class LezioneDao implements IlezioneDao {
   // Metodo che restituisce una lezione in base all'id passato come parametro
   @Override
   public synchronized LezioneBean doRetrieveLezioneById(int id)
@@ -29,8 +30,8 @@ public class LezioneDAO implements ILezioneDAO {
     LezioneBean bean = null;
     PreparedStatement stmt = null;
     String query =
-        "SELECT * from easyaid.lezione L inner join tutorato_didattico T " +
-                "where L.tutorato = T.idtutorato_didattico AND L.id ="
+        "SELECT * from easyaid.lezione L inner join tutorato_didattico T "
+                + "where L.tutorato = T.idtutorato_didattico AND L.id ="
             + id;
     try {
       conn = conn();
@@ -69,8 +70,8 @@ public class LezioneDAO implements ILezioneDAO {
     PreparedStatement stmt = null;
     Collection<LezioneBean> lezioni = new ArrayList<>();
     String query =
-        "SELECT * from easyaid.lezione L inner join tutorato_didattico T " +
-                "where L.tutorato = T.idtutorato_didattico AND L.tutorato ="
+        "SELECT * from easyaid.lezione L inner join tutorato_didattico T "
+                + "where L.tutorato = T.idtutorato_didattico AND L.tutorato ="
             + id;
     try {
       conn = conn();
@@ -112,8 +113,9 @@ public class LezioneDAO implements ILezioneDAO {
     PreparedStatement stmt = null;
     Collection<LezioneBean> lezioni = new ArrayList<>();
     String query =
-        "SELECT * from easyaid.lezione L inner join tutorato_didattico T " +
-                "where L.tutorato = T.idtutorato_didattico AND T.idtutorato_didattico = ? AND L.status = 1";
+        "SELECT * from easyaid.lezione L inner join tutorato_didattico T "
+                + "where L.tutorato = T.idtutorato_didattico "
+                + "AND T.idtutorato_didattico = ? AND L.status = 1";
     try {
       conn = conn();
       stmt = conn.prepareStatement(query);
@@ -147,6 +149,7 @@ public class LezioneDAO implements ILezioneDAO {
   }
   // Metodo che restituisce tutte le lezioni di un determinato studente il quale è stato passato
   // come parametro
+
   @Override
   public synchronized Collection<LezioneBean> doRetrieveLezioneByStudente(String studente)
       throws ClassNotFoundException, SQLException {
@@ -154,7 +157,11 @@ public class LezioneDAO implements ILezioneDAO {
     Collection<LezioneBean> lezioni = new ArrayList<>();
     PreparedStatement stmt = null;
     String query =
-        "SELECT * from lezione L inner join tutorato_didattico T inner join studente s where L.tutorato = T.idtutorato_didattico AND s.email_studente = T.studente_email AND s.email_studente =?";
+        "SELECT * from lezione L inner join tutorato_didattico T"
+                + " inner join studente s "
+                + "where L.tutorato = T.idtutorato_didattico"
+                + " AND s.email_studente = T.studente_email "
+                + "AND s.email_studente =?";
     try {
       conn = conn();
       stmt = conn.prepareStatement(query);
@@ -196,7 +203,9 @@ public class LezioneDAO implements ILezioneDAO {
     Collection<LezioneBean> lezioni = new ArrayList<>();
     PreparedStatement stmt = null;
     String query =
-        "SELECT * from lezione L inner join tutorato_didattico T inner join tutor v where L.tutorato = T.idtutorato_didattico AND v.email_tutor = T.tutor_email AND v.email_tutor=? ";
+        "SELECT * from lezione L inner join tutorato_didattico T "
+                + "inner join tutor v where L.tutorato = T.idtutorato_didattico "
+                + "AND v.email_tutor = T.tutor_email AND v.email_tutor=? ";
     try {
       conn = conn();
       stmt = conn.prepareStatement(query);
@@ -255,13 +264,14 @@ public class LezioneDAO implements ILezioneDAO {
   // Metodo che restituisce un booleano
   // true = Se la somma delle ore delle lezioni sono uguali alle ore richieste
   // false = Se la somma delle ore delle lezioni non sono uguali alle ore richieste
+
   @Override
   public synchronized int countOre(Collection<LezioneBean> lezioni, int idTutorato)
       throws SQLException, ClassNotFoundException {
     int uguale = -1;
     int count = 0;
     List<LezioneBean> lezioniS = (List<LezioneBean>) lezioni;
-    TutoratoDidatticoDAO dao = new TutoratoDidatticoDAO();
+    TutoratoDidatticoDao dao = new TutoratoDidatticoDao();
     if (idTutorato == 0) {
       idTutorato = doRetrieveTutoratoByLezione(lezioniS.get(0).getId());
     }
@@ -301,8 +311,9 @@ public class LezioneDAO implements ILezioneDAO {
     boolean utente = false;
     Connection conn = null;
     String query =
-        "INSERT INTO `easyaid`.`lezione`" +
-                " (`tutorato`, `ora_inizio`, `ora_fine`, `data`, `tutor`,`luogo`, `status`) VALUES (?,?,?,?,?,?,'0');";
+        "INSERT INTO `easyaid`.`lezione`"
+                + " (`tutorato`, `ora_inizio`, `ora_fine`, `data`, `tutor`,`luogo`, `status`)"
+                + " VALUES (?,?,?,?,?,?,'0');";
     PreparedStatement stmt = null;
     // Se riesce a connettersi, la connessione è != da null ed entra nello statement
     try {
